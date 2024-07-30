9c9ad26eacaf7024e8c14587f7a4a03fae986015
teslaDev18 <134542854+teslaDev18@users.noreply.github.com>
2024-07-30 07:21:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
+import {Person} from "@material-ui/icons"
+import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
+function App() {
+  return <Home/>
+}
+
+export default App;
